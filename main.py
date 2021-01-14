293d2d175181fd3c46d44c6f889f6a2e62be73ad
Aura <aurore.ducap@gmail.com>
2021-01-14 13:44:29 +0400

    utilisation de flake8-black

--- main.py
+++ main.py
@@ -1,27 +1,15 @@
 import book_info as bkinfo
 import categories as cat
 
-base_url = 'http://books.toscrape.com/'
+base_url = "http://books.toscrape.com/"
 
-if __name__ == '__main__':
-		
-	url_category = cat.url_category(base_url)
-	for url in url_category:
-		book_category = cat.get_category_url(url)
-		
-		for book in book_category:
-			book = bkinfo.get_book_info(book)
-			bkinfo.create_csv(book)
-			bkinfo.images(book)
+if __name__ == "__main__":
 
-		
+    url_category = cat.url_category(base_url)
+    for url in url_category:
+        book_category = cat.get_category_url(url)
 
-	
-	
-		
-
-
-
-	
-
-
+        for book in book_category:
+            book = bkinfo.get_book_info(book)
+            bkinfo.create_csv(book)
+            bkinfo.images(book)
